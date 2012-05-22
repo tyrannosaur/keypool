cca3dc2f8ef0721e4c07be579dd42121062bd9e9
Charlie Liban <charlie@tyrannosaur.ca>
2012-05-22 13:25:06 -0400

    Added additional metadata

--- python/setup.py
+++ python/setup.py
@@ -1,18 +1,38 @@
 ﻿#!/usr/bin/env python
 
-from distutils.core import setup
 from keypool import __version__
 
-setup(name='keypool',
-      version=__version__,
-      description='keypool: generate dict keys',
-      author='Charlie Liban',
-      author_email='charlie@tyrannosaur.ca',
-      maintainer='Charlie Liban',
-      maintainer_email='charlie@tyrannosaur.ca',
-      url='https://github.com/tyrannosaur/keypool',
-      packages=['keypool'],
-      long_description="Classes and helpers to generate and maintain a pool of unique integer keys.",
-      license="MIT License",
-      platforms=["any"],
-     )
+try:
+   from setuptools import setup
+   kw = {}
+
+except ImportError:
+   from distutils.core import setup
+   kw = {}
+
+setup(
+   name = 'keypool',
+   packages = ['keypool'],
+   version = __version__,
+   description = 'keypool: generate dict keys',
+
+   author = 'Charlie Liban',
+   author_email = 'charlie@tyrannosaur.ca',
+   maintainer='Charlie Liban',
+   maintainer_email='charlie@tyrannosaur.ca',   
+
+   url = 'https://github.com/tyrannosaur/keypool',
+   download_url = 'https://github.com/tyrannosaur/keypool/zipball/master',
+   keywords = ['data structures'],
+   classifiers = [
+      'Programming Language :: Python',
+      'License :: OSI Approved :: MIT License',
+   ],
+
+   license = 'MIT License',
+
+   long_description = """\
+Classes and helpers to generate and maintain a pool of unique integer keys.
+""",
+   **kw
+)     
