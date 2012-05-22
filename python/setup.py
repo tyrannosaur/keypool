af41d4db523682b3619b750610a2425d4d84906c
Charlie Liban <charlie@tyrannosaur.ca>
2012-05-22 13:32:23 -0400

    Changed documentation a bit

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ setup(
    name = 'keypool',
    packages = ['keypool'],
    version = __version__,
-   description = 'keypool: generate dict keys',
+   description = 'Classes and helpers to generate and maintain a pool of unique integer keys.',
 
    author = 'Charlie Liban',
    author_email = 'charlie@tyrannosaur.ca',
@@ -32,7 +32,25 @@ setup(
    license = 'MIT License',
 
    long_description = """\
-Classes and helpers to generate and maintain a pool of unique integer keys.
+Classes and helpers to generate and maintain a pool of unique integer keys.  
+Priority is given to reusing freed keys rather than generating new ones.
+
+This package is meant for situations where keys for a dict are irrelevant or
+arbitrary.
+
+Typical usage:
+    
+   from keypool import KeypoolDict
+   items = KeypoolDict()
+   
+   # Assign a value with a unique, generated key
+   items[items.next()] = 'hello, world'
+   
+   # Assign a value but capture the key
+   key = items.setitem('hello again, world')
+   
+   # Assign anything except an integer, like a normal dict
+   items['hello'] = 'world'
 """,
    **kw
 )     
